00bc4f33e5ca62569845b9f49442792e0fe4cf34
logicinfo <anselmo.lucas@hotmail.com>
2023-09-08 20:51:40 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Editor from './components/editor';
+
+function App() {
+  return (
+    <div className="Container">
+      <h1>Editor de textos</h1>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
